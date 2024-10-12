b7cee4e2d23818def966d06d36f7abbba4e65a4d
Duc - Xuân Đức Nguyễn <nguyenxuanduc1999@gmail.com>
2024-10-12 14:32:49 +0200

    first commit

---
+++ 03-computed/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { nextTick, reactive, ref } from 'vue'
+
+const book = reactive({
+  name: 'Book 1'
+  chapters: []
+
+})
+
+</script>
+
+<template>
+  <div>
+    <p id="count">Book name: {{ book.name }}</p>
+    <p id="count">Da xuat ban: {{ book.chapters.length > 9 ? 'co' : 'khong' }}</p>
+
+  </div> 
+
+</template>
